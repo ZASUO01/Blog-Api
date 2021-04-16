1de7d8ebacd5e9861e9413ec654ea0cd09536670
ZASUO01 <souzadraganeelc@gmail.com>
2021-04-15 22:29:20 -0300

    cors bug fixed

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.unsubscribe(cors());
+app.use(cors());
 
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
